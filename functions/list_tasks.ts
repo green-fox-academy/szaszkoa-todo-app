e159bdbe2390cab0cf9e0cd966acd59253c446ba
Szaszko <szaszkoa@gmail.com>
2019-10-03 14:31:55 +0200

    prepared list tasks for the completed/not completed part

--- functions/list_tasks.ts
+++ functions/list_tasks.ts
@@ -8,7 +8,7 @@ const fs = require('fs');
 export function list_task(){
   let items: string[] =  fs.readFileSync('./data/items.txt','utf-8').split('\r\n');
   for(let i: number = 0; i<items.length;i++){
-    items[i] = (i+1)+'. '+items[i];
+    items[i] = (i+1)+'. [ ] '+items[i];
   }
   console.log(`${header}\nYour items in your list are the following:\n\n${items.join('\r\n')}`);
 }
