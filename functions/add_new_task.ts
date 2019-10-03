7641b6f000d785986d339d0fa4c3838e21a73f34
Szaszko <szaszkoa@gmail.com>
2019-10-03 16:03:30 +0200

    handling completion status with existing functions

--- functions/add_new_task.ts
+++ functions/add_new_task.ts
@@ -6,8 +6,13 @@ import { args } from '../main';
 const fs = require('fs');
 
 export function add_new_task(){
+  // adding the text and number
   let items: string[] =  fs.readFileSync('./data/items.txt','utf-8').split('\r\n');
   items.push(args[3]);
   fs.writeFileSync('./data/items.txt',items.join('\r\n'));
+  // adding the completion status to the completion.txt
+  let completed: string[] =  fs.readFileSync('./data/completion.txt','utf-8').split('\r\n');
+  completed.push('false');
+  fs.writeFileSync('./data/completion.txt',completed.join('\r\n'));
   console.log('\nYour item have been added!');
 }
--- functions/remove_task.ts
+++ functions/remove_task.ts
@@ -6,8 +6,13 @@ import { args } from '../main';
 const fs = require('fs');
 
 export function remove_task(){
+  //removing the new item
   let items: string[] =  fs.readFileSync('./data/items.txt','utf-8').split('\r\n');
   items.splice(args[3]-1,1);
   fs.writeFileSync('./data/items.txt',items.join('\r\n'));
+  //removing the completion status from completion.txt
+  let completed: string[] =  fs.readFileSync('./data/completion.txt','utf-8').split('\r\n');
+  completed.splice(args[3]-1,1);
+  fs.writeFileSync('./data/completion.txt',completed.join('\r\n'));
   console.log(`\nItem No.${args[3]} have been removed from the list!`);
 }
