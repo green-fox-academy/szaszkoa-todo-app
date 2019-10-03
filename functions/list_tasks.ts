cd7c6870eefb66302ba3026f49f2c9712f033684
Szaszko <szaszkoa@gmail.com>
2019-10-03 16:54:45 +0200

    added empty list handling

--- functions/list_tasks.ts
+++ functions/list_tasks.ts
@@ -7,8 +7,14 @@ const fs = require('fs');
 export function list_task() {
   let items: string[] = fs.readFileSync('./data/items.txt', 'utf-8').split('\r\n');
   let completed: string[] = fs.readFileSync('./data/completion.txt', 'utf-8').split('\r\n');
-  for (let i: number = 0; i < items.length; i++) {
-    items[i] = (i + 1) + '. ' + (completed[i] === 'true' ? '[X] ' : '[ ] ') + items[i];
+  // checking for empty list
+  if (items[0] == "") {
+    console.log('\nYou are all done for today :)')
+  } else {
+    // printing all the tasks
+    for (let i: number = 0; i < items.length; i++) {
+      items[i] = (i + 1) + '. ' + (completed[i] === 'true' ? '[X] ' : '[ ] ') + items[i];
+    }
+    console.log(`${header}\nYour items in your list are the following:\n\n${items.join('\r\n')}`);
   }
-  console.log(`${header}\nYour items in your list are the following:\n\n${items.join('\r\n')}`);
 }
