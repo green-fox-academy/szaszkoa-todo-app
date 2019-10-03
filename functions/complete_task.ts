ed125d3c07041bf0025b27f7e5c32050a2dc0e46
Szaszko <szaszkoa@gmail.com>
2019-10-03 16:28:04 +0200

    changed the logic for changing the status

--- functions/complete_task.ts
+++ functions/complete_task.ts
@@ -6,8 +6,7 @@ const fs = require('fs');
 
 export function complete(){
   let completion_status: boolean[] =  fs.readFileSync('./data/completion.txt','utf-8').split('\r\n');
-  completion_status.splice(args[3]-1,1)
-  completion_status.push(true);
+  completion_status[args[3]-1] = true;
   fs.writeFileSync('./data/completion.txt',completion_status.join('\r\n'));
   console.log(`\nTask No.${args[3]} have been completed!`);
 }
